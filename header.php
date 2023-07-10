86dd339847d1d2bde50cd4e7ee74cf6d6b36a2ba
Nayandheke <119155513+Nayandheke@users.noreply.github.com>
2023-07-10 07:42:48 +0545

    second commit

---
+++ header.php
@@ -0,0 +1,32 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./dash.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <img class="LOGO" src="./img/logo-removebg-preview.png" alt="Logo">
+        </div>
+        <div class="buttons">
+          <a href="./login.php"> <button class="button1">Log In</button></a>
+          <a href="./register.php"> <button class="button1">Register</button></a>
+        </div>
+    </header>
+
+    <div class="container1">
+        <nav class="row">
+          <ul class="van">
+              <li><button class="button0">Home</button></li>
+              <li><button class="button0">About Us</button></li>
+              <li><button class="button0">Contact Us</button></li>
+              <a href="./data.php" class="data"><li><li><button class="button0">Data</button></li></li></a>
+          </ul>
+      </nav>
+    </div>
+</body>
+</html>
